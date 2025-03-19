f977234bc7287c611b74727ea55123ddf68a8d15
Nora_de_Greeuw <146176774+Nx27-ma@users.noreply.github.com>
2025-03-19 14:11:55 +0100

    template typescript react router

---
+++ nxbime/src/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Main: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to My Page</h1>
+            <p>This is a basic TSX page.</p>
+        </div>
+    );
+};
+
+export default Main;
